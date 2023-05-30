d69fb2e510c89bd7176ec0ac02476e1b795b21fb
048 -BayuSamodera <bayusamodera7@gmail.com>
2023-05-30 22:57:58 +0700

    fix: view kontak

--- application/views/home/kontak/view_kontak.php
+++ application/views/home/kontak/view_kontak.php
@@ -5,7 +5,7 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-body mb-5">
-                            <h3 class="my-5 text-center text-bold">Kontak Kami</h3>
+                            <h3 class="my-5 text-center text-bold">Kontak Kami Disini</h3>
                             <iframe class="responsive-iframe" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d253821.1530323931!2d106.4996154!3d-6.2695808!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f123cfd4ecc9%3A0x9862dee27f71c6da!2sPapalova%20Headquarter!5e0!3m2!1sid!2sid!4v1671545446176!5m2!1sid!2sid"></iframe>
                         </div>
                         <div class="d-flex align-items-center justify-content-center text-center mb-5">
